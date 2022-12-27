c67c201445823066bb4611af4b9368816294c3ec
Lychee <649540841@qq.com>
2022-12-27 23:05:51 +0800

    Site updated: 2022-12-27 23:05:49

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -1,15 +1,14 @@
-// 浏览器搞笑标题
- var OriginTitle = document.title;
+var OriginTitle = document.title;
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = 'ヽ(●-`Д´-)ノ你丑你就走！';
+         $('[rel="icon"]').attr('href', "/joke.ico");
+         document.title = '❗╭(°A°`)╮ 页面崩溃啦 ~';
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = 'ヾ(Ő∀Ő3)ノ你帅就回来！' + OriginTitle;
+         $('[rel="icon"]').attr('href', "/favicon.ico");
+         document.title = ' ✔(ฅ>ω<*ฅ) 噫又好了~';
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
